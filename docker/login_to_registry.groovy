b9ce5df05f01f8993406cd5c7e1e4c6aedc670d8
aleckeller <alec.keller.17@gmail.com>
2018-12-14 13:10:32 -0500

    Update login_to_registry.groovy

--- docker/login_to_registry.groovy
+++ docker/login_to_registry.groovy
@@ -10,7 +10,7 @@ void call(){
   def image_repo_cred = pipelineConfig?.application_image_repository_credential ?:
                         {error "application_image_repository_credential not defined in pipeline config"}()
   
-  withCredentials([usernamePassword(credentialsId: image_repo_cred, passwordVariable: 'pass', usernameVariable: 'user')]) {
+  withCredentials([usernamePassword(credentialsId: image_repo_cred, usernameVariable: 'user', passwordVariable: 'pass')]) {
     sh "echo username:${user}"
     sh "echo pass:${pass}"
     sh "echo ${pass} | docker login -u ${user} --password-stdin ${image_repo}"
