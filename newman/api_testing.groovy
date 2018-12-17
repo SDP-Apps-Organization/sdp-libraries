7263967237bc19113e0535c407fb194e4fe9f15d
Alec Keller <keller_alec@bah.com>
2018-12-17 10:43:38 -0500

    newman changes

--- newman/api_testing.groovy
+++ newman/api_testing.groovy
@@ -1,3 +1,12 @@
 def call(){
-  echo "running newman tests"
+  stage "API Testing", {
+    run_tests = config.dev_Run_Newman_Tests ?:
+          false
+    if (run_tests){
+      echo "running api tests"
+    }
+    else{
+      echo "skipping running api tests"
+    }
+  }
 }
