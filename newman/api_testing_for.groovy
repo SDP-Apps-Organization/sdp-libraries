d833e878103afc434a496318a550084fe90eee12
Alec Keller <keller_alec@bah.com>
2018-12-17 15:31:35 -0500

    test

--- newman/api_testing_for.groovy
+++ newman/api_testing_for.groovy
@@ -1,6 +1,8 @@
 void call(app_env){
   stage "API Testing", {
     env_variable = app_env.appName + "_Run_Newman_Tests"
+    echo "env varaible: " + env_variable
+    echo "config: " + config.env_variable
     run_tests = config.env_variable ?:
           false
     if (run_tests){
