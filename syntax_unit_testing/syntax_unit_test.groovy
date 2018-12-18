4bf7b0ddf34afc43000581d9c0d6e4fa7bdaa733
Alec Keller <keller_alec@bah.com>
2018-12-18 12:58:12 -0500

    fixed

--- syntax_unit_testing/syntax_unit_test.groovy
+++ syntax_unit_testing/syntax_unit_test.groovy
@@ -6,8 +6,7 @@ void call(app_env){
       def syntax_command = ""
       if (fileExists("package.json")){
         img = "node"
-        //unit_command = "npm install --only=dev; npm test"
-        unit_command = "ls"
+        unit_command = "npm install --only=dev; npm test"
         syntax_command = "/bin/bash -c \'walk() { cd \"\$1\"; for file in *; do if [ -d \"\$file\" ] && [ \"\$file\" != \"node_modules\" ]; then walk \"\$1/\$file\"; else if [[ ${file: -3} == \".js\" ]]; then node -c \"\$file\"; fi; fi; cd \"\$1\"; done; }; walk `pwd`\'"
       }
       else if (fileExists("pom.xml")){
