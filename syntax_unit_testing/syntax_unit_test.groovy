c28f38189e773de7f19c9ede9d8bc30f4ecfe4d3
Alec Keller <keller_alec@bah.com>
2018-12-18 13:25:32 -0500

    added ability to skip unit tests

--- syntax_unit_testing/syntax_unit_test.groovy
+++ syntax_unit_testing/syntax_unit_test.groovy
@@ -1,9 +1,11 @@
 void call(app_env){
-  stage "Unit & Syntax Testing", {
+  stage "Unit and Syntax Testing", {
     node {
       def img = ""
       def unit_command = ""
       def syntax_command = ""
+      def env_variable = app_env.appName + "_Skip_Tests"
+      def skip_unit_tests = config[env_variable] ?: false
       if (fileExists("package.json")){
         img = "node"
         unit_command = "npm install --only=dev; npm test"
@@ -19,8 +21,13 @@ void call(app_env){
       }
       docker.image(img).inside{
        unstash "workspace"
-       echo "Running Unit Tests..."
-       sh unit_command
+       if (!skip_unit_tests){
+         echo "Running Unit Tests..."
+         sh unit_command
+       }
+       else{
+         echo "Skipping running Unit Tests"
+       }
        echo "Running Syntax Tests..."
        sh syntax_command
       }
