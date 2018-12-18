0cb43bd2ae1ff7fa5a551c98f04629d5fae0aee3
Alec Keller <keller_alec@bah.com>
2018-12-18 11:39:47 -0500

    unit testing

--- unit_testing/unit_test.groovy
+++ unit_testing/unit_test.groovy
@@ -1,22 +1,24 @@
 void call(app_env){
   stage "Unit Testing", {
-    def img = ""
-    def command = ""
-    if (fileExists("package.json")){
-      img = "node"
-      command = "npm install --only=dev; npm test"
-    }
-    else if (fileExists("pom.xml")){
-      img = "maven"
-      command = "mvn test -Dmaven.test.failure.ignore=false"
-    }
-    else{
-      error "Unable to determine language: Must have a package.json or pom.xml"
-    }
+    node {
+      def img = ""
+      def command = ""
+      if (fileExists("package.json")){
+        img = "node"
+        command = "npm install --only=dev; npm test"
+      }
+      else if (fileExists("pom.xml")){
+        img = "maven"
+        command = "mvn test -Dmaven.test.failure.ignore=false"
+      }
+      else{
+        error "Unable to determine language: Must have a package.json or pom.xml"
+      }
 
-    docker.image(img).inside{
-     unstash "workspace"
-     sh command
+      docker.image(img).inside{
+       unstash "workspace"
+       sh command
+      }
     }
   }
 }
