c2daadd92bef6007d636d492322c0e98b3f7ea33
Alec Keller <keller_alec@bah.com>
2019-01-09 16:18:51 -0500

    added retag

--- docker/deploy_to.groovy
+++ docker/deploy_to.groovy
@@ -1,5 +1,7 @@
 void call(app_env){
   stage "Deploy to ${app_env.long_name}", {
-    authenticate_to_marketplace()
+    //authenticate_to_marketplace()
+    retag(env.GIT_SHA,app_env.short_name)
+    echo "Deploying with tag ${app_env.short_name}"
   }
 }
