362aa30fa82fb816c24fe8fba7b6ed3f15d09077
aleckeller <alec.keller.17@gmail.com>
2019-01-09 16:29:54 -0500

    Update deploy_to.groovy

--- docker/deploy_to.groovy
+++ docker/deploy_to.groovy
@@ -2,6 +2,6 @@ void call(app_env){
   stage "Deploy to ${app_env.long_name}", {
     //authenticate_to_marketplace()
     retag(env.GIT_SHA,app_env.short_name)
-    echo "Deploying with tag ${app_env.short_name}"
+    ls
   }
 }
