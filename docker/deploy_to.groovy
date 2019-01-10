44131590e74e34a8d47ac953a51c6f7ca15e99eb
Alec Keller <keller_alec@bah.com>
2019-01-10 10:11:35 -0500

    added compose

--- docker/deploy_to.groovy
+++ docker/deploy_to.groovy
@@ -2,11 +2,11 @@ void call(app_env){
   stage "Deploy to ${app_env.long_name}", {
     node {
       unstash "workspace"
-      
+
       //authenticate_to_marketplace()
       retag(env.GIT_SHA,app_env.short_name)
       sh "cat docker-compose.yml"
-      sh "docker ps"
+      sh "docker-compose -f docker-compose.yml up -d"
     }
   }
 }
